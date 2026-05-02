4aceb9ec865013a0f8d336a1814e0928702c91de
tamarajp3 <tamarajane.tp@gmail.com>
2026-05-02 02:41:19 +0000

    feat: add basic API post and get requests

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,30 @@
 from fastapi import FastAPI
+from core import Task
+from scheduler import optimal_schedule
 from pydantic import BaseModel
 
 
 app = FastAPI()
 
+tasks = []
+
+@app.get("/tasks"):
+def get_tasks():
+    return (t.taskname for t in tasks)
+
+@app.post("/tasks"):
+def create_task(task : createTask):
+    new = Task(
+        task.task_name
+        task.assessment_type_multiplier
+        task.deadline = datetime
+        task.weighing = weighing
+        task.estimated_time = estimated_time
+    )
+    tasks.append(new)
+
+@app.post("/schedule"):
+def get_schedule(request: scheduleRequest):
+    return optimal_schedule(tasks, request.hours_per_day)
+
+    
